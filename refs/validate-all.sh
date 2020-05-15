447bcbc4eb4d10a1981756581312beb348b92d25
kwatsen <kent@watsen.net>
2020-05-14 22:40:55 -0400

    added missing 'implemented' yang module to validate-all.sh

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -51,7 +51,7 @@ rm $name
 printf "Testing ex-http-client-proxy.xml..."
 name=`ls -1 ../ietf-http-client\@*.yang | sed 's/\.\.\///'`
 sed 's/^}/container http-client { uses http-client-grouping; }}/' ../ietf-http-client\@*.yang > $name
-command="yanglint -m -s -p ../ ./ietf-origin.yang $name ex-http-client-proxy.xml ../../trust-anchors/refs/ex-truststore.xml ../../keystore/refs/ex-keystore.xml" 
+command="yanglint -m -s -p ../ ./ietf-origin.yang ../ietf-crypto-types\@*.yang $name ex-http-client-proxy.xml ../../trust-anchors/refs/ex-truststore.xml ../../keystore/refs/ex-keystore.xml" 
 #command="yanglint -m -s ../ietf-crypto-types\@*.yang ../ietf-truststore\@*.yang ../ietf-keystore\@*.yang ../ietf-tls-common\@*.yang ./ietf-origin.yang $name ex-http-client-proxy.xml ../../trust-anchors/refs/ex-truststore.xml ../../keystore/refs/ex-keystore.xml"
 run_unix_cmd $LINENO "$command" 0
 printf "okay.\n"
